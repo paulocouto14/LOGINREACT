49ad65176c203d685d0abe4d69fa4283bf97bfe4
ppcouto <paulocouto14@gmail.com>
2023-01-10 13:50:10 -0300

    primeiro git

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>
+        tela react!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
